04a856a68701ff2f24c5cd7f48ef0346ad5439aa
Abdulla <abdullamagomedov9036@gmail.com>
2024-04-22 09:35:31 +0300

    Изменения

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+double a = Convert.ToDouble(Console.ReadLine());
+char b = Convert.ToChar(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+switch (b)
+{
+    case '+':
+        Console.WriteLine(a + c);
+        break;
+    case '-':
+        Console.WriteLine(a - c);
+        break;
+    case '*':
+        Console.WriteLine(a * c);
+        break;
+    case '/':
+        Console.WriteLine(a / c);
+        break;
+    default:
+        Console.WriteLine("Ошибка!");
+        break;
+}
+  
